585a054b8dd910b53cada3911a0b1d8442d53226
Thomas Levine <_@thomaslevine.com>
2014-07-05 16:42:11 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from distutils.core import setup
 
-from aamoer import __version__, __author__
-
 setup(name='aamoer',
-      author=__author__,
+      author='Thomas Levine',
       author_email='_@thomaslevine.com',
       description='Run statistics on data tables.',
       url='https://github.com/tlevine/aamoer',
       packages=['aamoer'],
       tests_require = ['nose'],
-      version=__version__,
+      version='0.0.1',
       license='AGPL',
 )
