83fbd7b831b877b1bcf06ad86406f74d14ad61a5
Thomas Levine <_@thomaslevine.com>
2014-07-06 01:17:09 -0400

    simpler cli function

--- aamoer/__init__.py
+++ aamoer/__init__.py
@@ -17,12 +17,10 @@ def cli(functions:dict):
     import sys, argparse, csv
     p = argparse.ArgumentParser(description = 'Find risk-related features in CSV files.')
     p.add_argument('csv', metavar = '[csv file]', nargs = '+', help = 'csv files to look at')
-    def main():
-        writer = csv.writer(sys.stdout)
-        writer.writerow(['filename','column.index','feature','value','count'])
-        for filename in p.parse_args().csv:
-            for column_number, column in enumerate(aamoer(functions, filename)):
-                for feature_name, counter in column.items():
-                    for value, value_count in counter.items():
-                        writer.writerow([filename,column_number,feature_name,value,value_count])
-    return main
+    writer = csv.writer(sys.stdout)
+    writer.writerow(['filename','column.index','feature','value','count'])
+    for filename in p.parse_args().csv:
+        for column_number, column in enumerate(aamoer(functions, filename)):
+            for feature_name, counter in column.items():
+                for value, value_count in counter.items():
+                    writer.writerow([filename,column_number,feature_name,value,value_count])
