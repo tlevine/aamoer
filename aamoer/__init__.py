c6c9b758eb107e917b3f04fe3518e2292066cd59
Thomas Levine <_@thomaslevine.com>
2014-07-14 06:56:22 -0400

    -

--- aamoer/__init__.py
+++ aamoer/__init__.py
@@ -17,9 +17,14 @@ def cli(functions:dict):
     import sys, argparse, csv
     p = argparse.ArgumentParser(description = 'Find risk-related features in CSV files.')
     p.add_argument('csv', metavar = '[csv file]', nargs = '+', help = 'csv files to look at')
+    if p.parse_args().csv == ['-']:
+        csv_files = (line.rstrip('\n') for line in sys.stdin)
+    else:
+        csv_files =  p.parse_args().csv
+
     writer = csv.writer(sys.stdout)
     writer.writerow(['filename','column.index','feature','value','count'])
-    for filename in p.parse_args().csv:
+    for filename in csv_files:
         for column_number, column in enumerate(aamoer(functions, filename)):
             for feature_name, counter in column.items():
                 for value, value_count in counter.items():
