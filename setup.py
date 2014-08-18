46a3e5a24b6741bb1e30208763616d664a72cef9
Thomas Levine <_@thomaslevine.com>
2014-08-18 15:19:41 +0000

    fix some stuff

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='aamoer',
       url='https://github.com/tlevine/aamoer',
       packages=['aamoer'],
       tests_require = ['nose'],
-      version='0.0.1',
+      version='0.0.2',
       license='AGPL',
 )
